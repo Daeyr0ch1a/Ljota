d91687c522f1aff6123bcc4af7ebe72c40195ad7
Daeyr0ch1a <157944146+Daeyr0ch1a@users.noreply.github.com>
2025-05-07 10:50:42 +0500

    07.05 --10:50

--- backend/database/config.py
+++ backend/database/config.py
@@ -1,9 +1,7 @@
-
-DB_USERNAME = "postgres"
-DB_PASSWORD = "5852"
-DB_HOST = "localhost"
+DB_USERNAME = "postgres.iiqqriuqssfweqzpzwfg"
+DB_PASSWORD = "5852104Zahan"
+DB_HOST = "aws-0-eu-north-1.pooler.supabase.com"
 DB_PORT = "5432"
-DB_NAME = "Ljota"
-
+DB_NAME = "postgres"
 
 DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
